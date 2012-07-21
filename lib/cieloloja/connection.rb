9e3b4c1431a927c99838dc9c93df193551104a2b
Giulliano Bueno <giulliano@e-processamento.com>
2012-07-21 02:53:14 -0300

    Forcando a nao verificacao

--- lib/cieloloja/connection.rb
+++ lib/cieloloja/connection.rb
@@ -6,11 +6,8 @@ module Cieloloja
       port = 443
       @http = Net::HTTP.new(@environment::BASE_URL,port)
       @http.use_ssl = true
-      if @environment.ssl_verify_mode == nil
-        @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      else
-        @http.verify_mode = @environment.ssl_verify_mode                
-      end
+      @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
       https.ca_path = '/etc/ssl/certs' if File.exists?('/etc/ssl/certs') # Ubuntu
       https.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt' if File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X      
       
