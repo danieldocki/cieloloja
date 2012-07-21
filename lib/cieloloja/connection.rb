f9221103a6f5235a5ae493fd25520e59fee5a425
Giulliano Bueno <giulliano@e-processamento.com>
2012-07-21 02:09:21 -0300

    Tentando corrigir o problema de ambiente no Ubuntu

--- lib/cieloloja/connection.rb
+++ lib/cieloloja/connection.rb
@@ -6,6 +6,14 @@ module Cieloloja
       port = 443
       @http = Net::HTTP.new(@environment::BASE_URL,port)
       @http.use_ssl = true
+      if @environment.ssl_verify_mode == nil
+        @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      else
+        @http.verify_mode = @environment.ssl_verify_mode                
+      end
+      https.ca_path = '/etc/ssl/certs' if File.exists?('/etc/ssl/certs') # Ubuntu
+      https.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt' if File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X      
+      
       @http.open_timeout = 10*1000
       @http.read_timeout = 40*1000
     end
@@ -16,6 +24,8 @@ module Cieloloja
         str_params+="&" unless str_params.empty?
         str_params+="#{key}=#{value}"
       end
+      
+      Rails.logger.info "Enviando XML para Cielo: #{str_params}"
       @http.request_post(self.environment::WS_PATH, str_params)
     end
   end
