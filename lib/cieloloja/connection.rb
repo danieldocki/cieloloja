881e6d300815710bc6fb02ce9ef0e2e298d0f1d2
Daniel Docki <daniel.docki@gmail.com>
2012-08-06 16:46:57 -0300

    remove ssl verify and certificate

--- lib/cieloloja/connection.rb
+++ lib/cieloloja/connection.rb
@@ -6,10 +6,10 @@ module Cieloloja
       port = 443
       @http = Net::HTTP.new(@environment::BASE_URL,port)
       @http.use_ssl = true
-      @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      # @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      @http.ca_path = '/etc/ssl/certs' if File.exists?('/etc/ssl/certs') # Ubuntu
-      @http.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt' if File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X      
+      # @http.ca_path = '/etc/ssl/certs' if File.exists?('/etc/ssl/certs') # Ubuntu
+      # @http.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt' if File.exists?('/opt/local/share/curl/curl-ca-bundle.crt') # Mac OS X      
       
       @http.open_timeout = 10*1000
       @http.read_timeout = 40*1000
